a476ae07e530a48cc1f783bf4cdd4d7fc1981633
Ivan Marchenko <51205960+IwalkInSilence@users.noreply.github.com>
2021-01-19 21:45:31 +0200

    implement Servlet with index.jsp Hello World!

---
+++ src/main/java/core/service/GenericService.java
@@ -0,0 +1,16 @@
+package core.service;
+
+import java.util.List;
+
+public interface GenericService<T, I> {
+    T create(T value);
+
+    T get(I id);
+
+    List<T> getAll();
+
+    T update(T value);
+
+    boolean delete(I id);
+}
+
